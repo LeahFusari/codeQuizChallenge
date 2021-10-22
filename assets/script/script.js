ba12141256b352dae86d12ea5283544cc2699be9
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-21 20:52:30 -0400

    Displayed questions and answer choices (buttons) and button click events.

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,12 @@
 var timeLeft = 10;
 var timerCountdown = 0;
 var questionNum = 0;
-var timeDisplay = document.querySelector("#timer")
+var index = 0;
+var timeDisplay = document.querySelector("#timer");
+var gameRulesSect = document.querySelector("#gameRules");
+var questionDisplay = document.querySelector("#questArea");
+var selections = document.querySelector("#choices");
+
 
 // Array of objects (questions, choices and correct answers) with a nested array of choices
 var questions = [
@@ -40,19 +45,64 @@ var questions = [
 // make start quiz button do something
 
 
-function startQuiz(){
+function startTimer(){
     var timerCountdown = setInterval(function(){
-        console.log(timeLeft);
         timeLeft--;
         timeDisplay.textContent = "Time Left: " + timeLeft + " seconds";
-            if (timeLeft === 0){
+            if (timeLeft === 0 || questions === questions.length){
                 clearInterval(timerCountdown);
             }
     },1000);
 };
 
+function displayQuestions(){
+    document.getElementById("qTitle").textContent = questions[index].title
+    selections.innerHTML=""
+    var button1=document.createElement("button")
+        button1.textContent = questions[index].choices[0]
+        button1.addEventListener("click", function(){
+            checkNext()
+        })
+        selections.appendChild(button1)
+
+    var button2=document.createElement("button")
+        button2.textContent = questions[index].choices[1]
+        button2.addEventListener("click", function(){
+            checkNext()
+        })
+        selections.appendChild(button2)
+
+    var button3=document.createElement("button")
+        button3.textContent = questions[index].choices[2]
+        button3.addEventListener("click", function(){
+            checkNext()
+        })
+        selections.appendChild(button3)
+
+    var button4=document.createElement("button")
+        button4.textContent = questions[index].choices[3]
+        button4.addEventListener("click", function(){
+            checkNext()
+        })
+        selections.appendChild(button4)
+}
+
+function checkNext(){
+
+    index++
+    displayQuestions()
+}
+
+function startGame(){
+    gameRulesSect.style.display="none"
+    
+    startTimer()
+    displayQuestions()
+
+
+}
 
 
 
 
-document.getElementById("startQuiz").addEventListener("click", startQuiz);
+document.getElementById("startQuiz").addEventListener("click", startGame);
