8c9b75a78f6828b3940035bfebf0a51da5d00a2e
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-22 15:47:42 -0400

    targeted selected button text content and question index

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 
-var timeLeft = 10;
+var timeLeft = 75;
 var timerCountdown = 0;
 var questionNum = 0;
 var index = 0;
@@ -61,42 +61,46 @@ function displayQuestions(){
     var button1=document.createElement("button")
         button1.textContent = questions[index].choices[0]
         button1.addEventListener("click", function(){
-            checkNext()
-        })
+            checkNext(button1.textContent,questions[index])
+            })
+        
         selections.appendChild(button1)
 
     var button2=document.createElement("button")
         button2.textContent = questions[index].choices[1]
         button2.addEventListener("click", function(){
-            checkNext()
-        })
+            checkNext(button2.textContent)
+            })
         selections.appendChild(button2)
 
     var button3=document.createElement("button")
         button3.textContent = questions[index].choices[2]
         button3.addEventListener("click", function(){
-            checkNext()
-        })
+            checkNext(button3.textContent)
+            })
         selections.appendChild(button3)
 
     var button4=document.createElement("button")
         button4.textContent = questions[index].choices[3]
         button4.addEventListener("click", function(){
-            checkNext()
-        })
+            checkNext(button4.textContent)
+            })
         selections.appendChild(button4)
 }
 
-function checkNext(event){
-    document.getElementById("choices").addEventListener("click",checkNext);
-        console.log(event.target.value);
+function checkNext(selectedButton,questNum){
+
+    // document.getElementById("choices").addEventListener("click",checkNext);
+        console.log(selectedButton);
+        console.log(questionNum);
     // if (questions[questionNum].correctAsr === target.value) {
     //     p.textContent = "Correct!";
     // } else if (questions[questionCount].correctAnswer !== event.target.value) {
     //     secondsLeft = secondsLeft - 10;
     //     p.textContent = "Wrong!";
     // }
-    index++
+    questionNum = index++
+    console.log(questionNum);
     displayQuestions()
 }
 
